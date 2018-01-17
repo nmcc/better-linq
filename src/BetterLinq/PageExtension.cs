10f0b1fc9a711a5ab3e7ee17f540a3d92eb4dfe8
Nuno Caneco <nuno.caneco@farfetch.com>
2018-01-17 14:07:45 +0000

    Implement Page<>
    Issue #1

---
+++ src/BetterLinq/PageExtension.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace BetterLinq
+{
+    /// <summary>
+    /// Implements the pagination extension.
+    /// </summary>
+    public static class PageExtension
+    {
+        /// <summary>
+        /// Paginates an enumeration.
+        /// </summary>
+        /// <typeparam name="T">The type of the items of the collection.</typeparam>
+        /// <param name="collection">The collection to be paginated.</param>
+        /// <param name="pageSize">The size of each page.</param>
+        /// <param name="pageNumber">The number of the page, starting at 1.</param>
+        /// <returns>The enumerable with the paginated data.</returns>
+        public static IEnumerable<T> Page<T>(this IEnumerable<T> collection, int pageSize, int pageNumber = 1)
+        {
+            if (collection == null)
+            {
+                return null;
+            }
+
+            if(pageNumber < 1)
+            {
+                throw new ArgumentOutOfRangeException(nameof(pageNumber), "Page number must be greater than 1");
+            }
+
+            if (pageSize < 1)
+            {
+                throw new ArgumentOutOfRangeException(nameof(pageSize), "Page size must be greater than 1");
+            }
+
+            return collection
+                .Skip(pageSize * (pageNumber - 1))
+                .Take(pageSize);
+        }
+    }
+}
