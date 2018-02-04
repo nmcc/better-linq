ff03dce8f6361c1bfa2d332231d46318d869d304
Alexandre Pires <afnpires@outlook.com>
2018-02-04 12:01:04 +0000

    feat(GroupByAggregate): create extension method and parameter validation
    Related to #11

---
+++ src/BetterLinq/GroupByAggregateExtension.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace BetterLinq
+{
+    /// <summary>
+    /// Implements the group by aggregate extension
+    /// </summary>
+    public static class GroupByAggregateExtension
+    {
+        public static IEnumerable<IGrouping<TKey, TElement>> GroupByAggregate<TSource, TKey, TElement>(
+            this IEnumerable<TSource> collection,
+            Func<TSource, TKey> keySelector,
+            Func<TSource, TElement> elementSelector)
+        {
+            if (collection == null)
+            {
+                return null;
+            }
+
+            if (keySelector == null)
+            {
+                throw new ArgumentNullException(nameof(keySelector));
+            }
+
+            if (elementSelector == null)
+            {
+                throw new ArgumentNullException(nameof(elementSelector));
+            }
+
+            throw new NotImplementedException();
+        }
+    }
+}
---
+++ test/BetterLinq.Tests/GroupByAggregateTests.cs
@@ -0,0 +1,41 @@
+﻿using FluentAssertions;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using Xunit;
+
+namespace BetterLinq.Tests
+{
+    public class GroupByAggregateTests
+    {
+        [Fact]
+        public void When_NullCollection_ReturnNull()
+        {
+            IEnumerable<int> collection = null;
+
+            collection.GroupByAggregate(x => x, x => x).Should().BeNull();
+        }
+
+        [Fact]
+        public void When_EmptyKeySelectorParameter_ThrowArgumentNullException()
+        {
+            IEnumerable<int> collection = new List<int>();
+
+            Action act = () => collection.GroupByAggregate<int, int, int>(null, x => x);
+
+            act.ShouldThrow<ArgumentNullException>()
+                .WithMessage("Value cannot be null.\r\nParameter name: keySelector");
+        }
+
+        [Fact]
+        public void When_EmptyElementSelectorParameter_ThrowArgumentNullException()
+        {
+            IEnumerable<int> collection = new List<int>();
+
+            Action act = () => collection.GroupByAggregate<int, int, int>(x => x, null);
+
+            act.ShouldThrow<ArgumentNullException>()
+                .WithMessage("Value cannot be null.\r\nParameter name: elementSelector");
+        }
+    }
+}
